7744c089a2c28a3c7c010871690c49a2c8c64d4b
N6-user <142050623+N6-user@users.noreply.github.com>
2024-02-27 16:11:07 -0300

    Primera versión
    Cuenta con las páginas de acceso a cuenta y selección de aula.

---
+++ JS/acceso_cuenta.js
@@ -0,0 +1,59 @@
+const USUARIO = document.getElementById("tf-usuario");
+const CONTRASEÑA = document.getElementById("pf-contraseña");
+const todosLosDatos = [USUARIO, CONTRASEÑA], erroresDatoIncompleto = [], erroresDatoEquivocado = [];
+
+function corroborarDatos() {
+    erroresDatoIncompleto.length = 0;
+    erroresDatoEquivocado.length = 0;
+    let usuario = USUARIO.value;
+    let contraseña = CONTRASEÑA.value;
+    let datosCorrectos = true;
+
+    // revisar usuario
+    if (usuario == "") {
+        datosCorrectos = false;
+        erroresDatoIncompleto.push(USUARIO);
+    } else {
+        if (usuario != "usuario") {
+            datosCorrectos = false;
+            erroresDatoEquivocado.push(USUARIO)
+        }
+    }
+
+    // revisar contraseña
+    if (contraseña == "") {
+        datosCorrectos = false;
+        erroresDatoIncompleto.push(CONTRASEÑA);
+    } else {
+        if (contraseña != "contraseña") {
+            datosCorrectos = false;
+            erroresDatoEquivocado.push(CONTRASEÑA);
+        }
+    }
+
+    if (datosCorrectos) {
+        window.location.replace("aulas.html");
+    } else {
+        mostrarErrores();
+    }
+}
+
+function mostrarErrores() {
+    const MENSAJE = document.getElementById("mensaje-form-usuario-contraseña");
+    // limpiar advertencia anterior si la hubiera
+    MENSAJE.innerHTML = "";
+
+    // colocar advertencia de ser necesario - usuario
+    if (erroresDatoIncompleto.length > 0) {
+        MENSAJE.innerHTML = "Debe completar ambos campos.";
+        return;
+    }
+
+    // colocar advertencia de ser necesario - contraseña
+    if (erroresDatoEquivocado.length > 0) {
+        MENSAJE.innerHTML = "Usuario y/o contraseña incorrectos. Por favor, vuelta a intentarlo.";
+    }
+}
+
+
+
---
+++ JS/seleccion_aula.js
@@ -0,0 +1,38 @@
+class Materia {
+    constructor(nombre, link, img) {
+        this.nombre = nombre;
+        this.link = link;
+        this.img = img;
+    }
+}
+
+const LABOTARORIO_PROGRAMACION_1 = new Materia("TUDS - 2023: Lab. de programación 1 - C2", "lab_programacion_1.html", "Imágenes/lab_programacion_1.png");
+const PROGRAMACION_WEB_1 = new Materia("TUDS - 2023: Programación Web 1 - C2", "programacion_web_1.html", "Imágenes/programacion_web_1.png");
+const MATEMATICA_1 = new Materia("TUDS - 2023: Matemática 1 - C2", "matematica_1.html", "Imágenes/matemática_1.png");
+const EDA = new Materia("TUDS - 2023: Estructura de datos y algoritmos - C2", "estructura_datos_algoritmos.html", "Imágenes/estructura_datos_algoritmos.png");
+
+const MATERIAS = [LABOTARORIO_PROGRAMACION_1, PROGRAMACION_WEB_1, MATEMATICA_1, EDA];
+const ANCLA_MATERIA = document.getElementById("link-materia");
+const IMG_MATERIA = document.getElementById("img-materia");
+const NOMBRE_MATERIA = document.getElementById("nombre-materia");
+let materiaActual = 0;
+
+function anteriorMateria() {
+    materiaActual = (materiaActual-- + MATERIAS.length - 1) % MATERIAS.length;
+    const MATERIA_MOSTRADA = MATERIAS[materiaActual];
+
+    ANCLA_MATERIA.href = MATERIA_MOSTRADA.link;
+    IMG_MATERIA.src = MATERIA_MOSTRADA.img;
+    IMG_MATERIA.alt = MATERIA_MOSTRADA.nombre;
+    NOMBRE_MATERIA.innerHTML = MATERIA_MOSTRADA.nombre;
+}
+
+function posteriorMateria() {
+    materiaActual = (materiaActual++ + 1) % MATERIAS.length;
+    const MATERIA_MOSTRADA = MATERIAS[materiaActual];
+
+    ANCLA_MATERIA.href = MATERIA_MOSTRADA.link;
+    IMG_MATERIA.src = MATERIA_MOSTRADA.img;
+    IMG_MATERIA.alt = MATERIA_MOSTRADA.nombre;
+    NOMBRE_MATERIA.innerHTML = MATERIA_MOSTRADA.nombre;
+}
