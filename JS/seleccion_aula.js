c0afa153fe60efaf2edb06b808fb6ae9144d8b19
N6-user <142050623+N6-user@users.noreply.github.com>
2024-02-29 14:37:59 -0300

    Segunda versión
    Se han agregado:
    -Menú vertical en el encabezado
    -Ruta de navegación en el encabezado
    -Páginas de aulas, con sus respectivas secciones
    -La base de la página de inscripción a materias (falta el formulario del mismo)

--- JS/seleccion_aula.js
+++ JS/seleccion_aula.js
@@ -6,11 +6,10 @@ class Materia {
     }
 }
 
-const LABOTARORIO_PROGRAMACION_1 = new Materia("TUDS - 2023: Lab. de programación 1 - C2", "lab_programacion_1.html", "Imágenes/lab_programacion_1.png");
-const PROGRAMACION_WEB_1 = new Materia("TUDS - 2023: Programación Web 1 - C2", "programacion_web_1.html", "Imágenes/programacion_web_1.png");
-const MATEMATICA_1 = new Materia("TUDS - 2023: Matemática 1 - C2", "matematica_1.html", "Imágenes/matemática_1.png");
-const EDA = new Materia("TUDS - 2023: Estructura de datos y algoritmos - C2", "estructura_datos_algoritmos.html", "Imágenes/estructura_datos_algoritmos.png");
-
+const LABOTARORIO_PROGRAMACION_1 = new Materia("TUDS - 2023: Lab. de programación 1 - C2", "lab_prog_1_seccion_principal.html", "Imágenes/lab_programacion_1.png");
+const PROGRAMACION_WEB_1 = new Materia("TUDS - 2023: Programación Web 1 - C2", "prog_web_1_seccion_principal.html", "Imágenes/programacion_web_1.png");
+const MATEMATICA_1 = new Materia("TUDS - 2023: Matemática 1 - C2", "mat_1_seccion_principal.html", "Imágenes/matemática_1.png");
+const EDA = new Materia("TUDS - 2023: Estructura de datos y algoritmos - C2", "eda_seccion_principal.html", "Imágenes/estructura_datos_algoritmos.png");
 const MATERIAS = [LABOTARORIO_PROGRAMACION_1, PROGRAMACION_WEB_1, MATEMATICA_1, EDA];
 const ANCLA_MATERIA = document.getElementById("link-materia");
 const IMG_MATERIA = document.getElementById("img-materia");
